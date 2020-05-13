770a274444909ebc061a135e71f38987fef5f328
neymarjimoh <jemohkunle2007@gmail.com>
2020-05-13 15:53:01 +0100

    feat: added the functionslity-nesletter

--- src/components/NewsLetter/NewsLetter.js
+++ src/components/NewsLetter/NewsLetter.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 // import PropTypes from 'prop-types'
 
 class componentName extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            email: ''
+        }
+    }
+    
+    handleChange = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+
+    }
 
     render() {
         return (
@@ -11,8 +31,24 @@ class componentName extends Component {
                         <h1 className="newsletter-div-col-1-heading">Join Our NewsLetter</h1>
                         <h6 className="newsletter-div-col-1-text">Subcsribe to stay up to date with our latest updates, features and discount prices</h6>
                         <div className="subscribe-form">
-                            <form onSubmit="">
-                                <input className="subscribe-form-input" type="email" placeholder="Enter Email.." onChange="" required />
+                            <form 
+                                action="https://gmail.us18.list-manage.com/subscribe/post?u=ffadc959838ecfa2cac50cd10&amp;id=36ceba5904" 
+                                method="post" 
+                                id="mc-embedded-subscribe-form" 
+                                name="mc-embedded-subscribe-form" 
+                                className="validate" 
+                                target="_blank" 
+                                novalidate
+                            >
+                                <input 
+                                    className="subscribe-form-input" 
+                                    type="email"
+                                    name="email" 
+                                    placeholder="Enter Email.." 
+                                    onChange={this.handleChange}  
+                                    value={this.state.email} 
+                                    required 
+                                />
                                 <button className="subscribe-form-button">SUBSCRIBE &nbsp; <img src="../../images/send-icon.png" alt="hh" /></button>
                             </form>
                         </div>
@@ -23,6 +59,8 @@ class componentName extends Component {
                         <p><img src="https://img.icons8.com/color/24/000000/gmail.png" alt="gmail-icon" /> jemohkunle2007@gmail.com</p>
                     </div>
                 </div>
+                Subscribe
+                <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
             </div>
         )
     }
